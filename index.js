02c01b26e1d537fc844cedfea6f9f1fb59aff9fb
picakhant <picakhant@gmail.com>
2025-01-15 19:26:38 +0630

    Update

--- index.js
+++ index.js
@@ -1,78 +1,48 @@
 import express from "express";
+import prisma from "./prismaClient.js";
 
-const app = express();
+const server = express();
 
-const myMiddleware = (req, res, next) => {
-  console.log("Passed Into middleware");
-  next();
-};
+server.use(express.json());
 
-// middleawre fro json data
-app.use(express.json());
-app.use(myMiddleware);
-
-const fakeData = {
-  id: 1,
-  name: "IPhone 11 pro",
-};
-
-app.get("/", (req, res) => {
-  return res.status(200).json(fakeData);
+server.get("/", (req, res) => {
+  return res.status(200).json({ message: "ok" });
 });
 
-app.post("/", (req, res) => {
-  // data from req body
-  const { name } = req.body;
+server.post("/create-book", async (req, res) => {
+  try {
+    const { name, author, release, price } = req.body;
 
-  // condition
-  if (!name) {
-    return res.status(400).json({ message: "Invalid Data" });
-  }
-
-  if (name.length < 2) {
-    return res.status(400).json({ message: "Name Must be at least 2 chars" });
-  }
-
-  return res.status(200).json({ message: "Ok" });
-});
-
-app.get(
-  "/about",
-  (req, res, next) => {
-    console.log("You Pass Into Middleware");
-    next();
-  },
-  (req, res) => {
-    return res.status(200).json({ message: "Ok" });
-  }
-);
-
-app.post(
-  "/admin/create-post",
-  // data valid middleware
-  (req, res, next) => {
-    const { id, title, desc, authorId, authorRole } = req.body;
-
-    if (!id || !title || !desc || !authorId || !authorRole) {
+    if (!name || !author || !release || !price) {
       return res.status(400).json({ message: "Invalid Data" });
     }
-    next();
-  },
-  //authorization middleware
-  (req, res, next) => {
-    const { authorRole } = req.body;
 
-    if (authorRole === "user") {
-      return res.status(403).json({ message: "Only admin can access!" });
+    const isNameAlreadyExist = await prisma.book.findUnique({
+      where: {
+        name,
+      },
+    });
+
+    if (isNameAlreadyExist) {
+      return res.status(400).json({ message: "Book name is already exist!" });
     }
-    next();
-  },
-  // final function
-  (req, res) => {
-    return res.status(200).json({ message: "Post Created Successful" });
-  }
-);
 
-app.listen(4000, () => {
-  console.log("App is running on port 4000");
+    await prisma.book.create({
+      data: {
+        name,
+        author,
+        release,
+        price,
+      },
+    });
+
+    return res.status(200).json({ message: "Book created sucessful!" });
+  } catch (error) {
+    console.log("Error is ", error.message);
+    return res.status(500).json({ message: "Interval server error!" });
+  }
 });
+
+//http://localhost:4000/creat-post
+
+server.listen(4000, () => console.log("Server is running on port 3000"));
--- read.md
+++ read.md
@@ -62,6 +62,71 @@ app.get("/route_name", (req, res, next) => {
 }, (req, res) => return res) // specific route
 ```
 
-# Day4 & Day 5
+# Day4 & Day 5 & Day 6
 
 ## Discuss what is relational database and prism set up
+
+# Day 7
+
+## handle database with express
+
+- Create Prisma Client
+
+```javascript
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
+```
+
+- Prisma Schema & Migrations
+
+```prisma
+model Book {
+  id      Int    @id @default(autoincrement())
+  name    String @unique
+  author  String
+  release String
+  price   Float
+}
+
+```
+
+- Handle db with express
+
+```js
+server.post("/create-book", async (req, res) => {
+  try {
+    const { name, author, release, price } = req.body;
+
+    if (!name || !author || !release || !price) {
+      return res.status(400).json({ message: "Invalid Data" });
+    }
+
+    const isNameAlreadyExist = await prisma.book.findUnique({
+      where: {
+        name,
+      },
+    });
+
+    if (isNameAlreadyExist) {
+      return res.status(400).json({ message: "Book name is already exist!" });
+    }
+
+    await prisma.book.create({
+      data: {
+        name,
+        author,
+        release,
+        price,
+      },
+    });
+
+    return res.status(200).json({ message: "Book created sucessful!" });
+  } catch (error) {
+    console.log("Error is ", error.message);
+    return res.status(500).json({ message: "Interval server error!" });
+  }
+});
+```
